ca4a7218d3eeda5aef6fa95dfc707c079dd6f970
Mykhailo Maidan <mykhailo.maidan@yalantis.net>
2023-12-27 18:20:50 +0100

    Add fetch

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,8 +5,8 @@ import { useState, FormEvent, ChangeEvent } from 'react'
 import styles from './signup.module.css'
 
 interface FormData{
-    userName: string;
-    userSurname: string;
+    name: string;
+    surname: string;
     email: string;
     password: string;
 }
@@ -14,8 +14,8 @@ interface FormData{
 export default function SignUp(){
 
     const [formData, setFormData] = useState<FormData>({
-        userName: '',
-        userSurname: '',
+        name: '',
+        surname: '',
         email: '',
         password: '',
 
@@ -32,6 +32,15 @@ export default function SignUp(){
       // Handle form submission
       const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
+        const result = fetch("http://localhost:4000/register",{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json', 
+          },
+          body: JSON.stringify(formData)
+        })
+        .then(response => console.log(response))
+        .catch(err => console.log(err))      
         // Handle your form submission logic here, e.g., send data to an API
         console.log(formData);
       };
@@ -41,15 +50,56 @@ export default function SignUp(){
             <h1 className={styles.formTitle}>SignUp</h1>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <input
-                    type='text'
-                    name='username'
-                    placeholder='Username'
-                    value={formData.userName}
-                    onChange={handleChange}
-                    className={styles.inputField}
-                    required
-                    />
+                  <input
+                  type='text'
+                  name='name'
+                  placeholder='Username'
+                  value={formData.name}
+                  onChange={handleChange}
+                  className={styles.inputField}
+                  required
+                  />
+                </div>
+                <div>
+                  <input
+                  type='text'
+                  name='surname'
+                  placeholder='Surname'
+                  value={formData.surname}
+                  onChange={handleChange}
+                  className={styles.inputField}
+                  required
+                  />
+                </div>
+                <div>
+                  <input
+                  type='text'
+                  name='email'
+                  placeholder='Email'
+                  value={formData.email}
+                  onChange={handleChange}
+                  className={styles.inputField}
+                  required
+                  />
+                </div>
+                 <div>
+                  <input
+                  type='text'
+                  name='password'
+                  placeholder='Password'
+                  value={formData.password}
+                  onChange={handleChange}
+                  className={styles.inputField}
+                  required
+                  />
+                </div>
+                <div>
+                  <button
+                  type='submit'
+                  className={styles.submitButton}
+                  >
+                    SignUp
+                  </button>
                 </div>
             </form>
         </div>
